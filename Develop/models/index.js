42ac8c97fdccc6fdc54df47658a04c320eaae672
taycannon <taycannon2003@gmail.com>
2023-10-27 17:28:07 -0500

    Added relationships between Models in index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_Id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey:'product_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag,{
+  foreignKey:'ProductTag'
+})
 // Tags belongToMany Products (through ProductTag)
-
+TimeRanges.belongsToMany(Product, {
+  foreignKey: 'ProductTag'
+})
 module.exports = {
   Product,
   Category,
