fa721e3bc7a5619ea3055a4d1d5cefbc8af161cf
taycannon <taycannon2003@gmail.com>
2023-10-27 17:43:19 -0500

    Fixed relationship to correct relationship & installed 3 depenedencies: mysql, sequelize, & dotenv + added to README

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,19 +7,25 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_Id'
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey:'product_id'
-})
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(ProductTag,{
-  foreignKey:'ProductTag'
-})
+  through: {
+    model: ProductTag,
+    foreignKey:'ProductTag',
+  }
+});
 // Tags belongToMany Products (through ProductTag)
 TimeRanges.belongsToMany(Product, {
-  foreignKey: 'ProductTag'
-})
+  through: {
+    model: ProductTag,
+    foreignKey:'tag_id',
+  }
+});
 module.exports = {
   Product,
   Category,
