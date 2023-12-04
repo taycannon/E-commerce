e4aea4f8d27c46374a392db58f6a1450396076fa
taycannon <taycannon2003@gmail.com>
2023-12-04 16:13:18 -0600

    Deleted very few lines of code in the models folder as well as added to the index.js in that folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,26 +6,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-});
-
+Product.belongsTo(Category,{
+  foreignKey:"category_id",
+  onDelete:'CASCADE'
+})
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id',
-});
-
+Category.hasMany(Product,{
+  foreignKey:"category_id",
+})
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id',
-});
+Product.belongsToMany(Tag,{
+  through:{
+  model:ProductTag,
+  unique: false,
+},
+as: 'product_tag_info',
+})
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
-  through: ProductTag, 
-  foreignKey: 'tag_id',
-});
-
+Tag.belongsToMany(Product,{
+  through:{
+    model:ProductTag,
+    unique: false,
+  },
+  as: 'tag_product_info',
+})
 module.exports = {
   Product,
   Category,
